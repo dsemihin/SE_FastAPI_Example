5ab36afd95e239caca5f6a8225d3f779a0274fae
David Chepak <chepak.dava@gmail.com>
2025-02-24 18:16:34 +0500

    Добавлены комментарии и изменен язык текста вывода в файле main.py

--- main.py
+++ main.py
@@ -2,25 +2,23 @@ from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
 
-
 class Item(BaseModel):
     text: str
-
 
 app = FastAPI()
 classifier = pipeline("sentiment-analysis")
 
-
 @app.get("/")
 def root():
-    return {"FastApi service started!"}
-
+    """Корневой эндпоинт для подтверждения работы сервиса."""
+    return {"сообщение": "Сервис FastAPI запущен!"}
 
 @app.get("/{text}")
 def get_params(text: str):
+    """Эндпоинт для получения анализа тональности текста."""
     return classifier(text)
-
 
 @app.post("/predict/")
 def predict(item: Item):
+    """Эндпоинт для предсказания тональности текста."""
     return classifier(item.text)
